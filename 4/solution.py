4ce2638abdcd34afad266e0b949e3682edafc5e1
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-04-02 11:45:40 +0200

    day 4 part 2

--- 4/solution.py
+++ 4/solution.py
@@ -7,7 +7,7 @@ with open("input.txt") as f:
 n = 0
 ha = md5(f'{inp}{n}'.encode("utf-8"))
 
-while ha.hexdigest()[0:5] != '00000':
+while ha.hexdigest()[0:6] != '000000':
     n += 1
     ha = md5(f'{inp}{n}'.encode("utf-8"))
 print(n ,ha.hexdigest(), ha.hexdigest()[0:6])
