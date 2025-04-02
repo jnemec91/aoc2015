b2cc1b04d7a214229eda65e5eab7b8f469dbd9af
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-04-02 10:20:18 +0200

    day 1 part 2

--- 1/solution.py
+++ 1/solution.py
@@ -1,4 +1,12 @@
 with open("input.txt") as f:
-    print(
-        sum([1 if i == '(' else -1 for i in f.read()])
-    )
+    floors = [1 if i == '(' else -1 for i in f.read()]
+    print(floors)
+
+    floor = 0
+    position = 0
+
+    while floor >= 0:
+        floor += floors[position]
+        position += 1
+        
+    print(position)
