a0464229a0e927cea7394b9526fd25522eeab509
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-04-02 11:21:19 +0200

    day 3 part 2

--- 3/solution.py
+++ 3/solution.py
@@ -4,18 +4,26 @@ with open("input.txt") as f:
     inp = f.read()
 
 locations = [(0,0)]
-x,y = 0, 0
 
-for i in inp:
-    if i == '>':
-        x+=1
-    elif i == '<':
-        x-=1
-    elif i == '^':
-        y-=1
-    elif i == 'v':
-        y+=1
 
-    locations.append((x,y))
+santa = [i for c, i in enumerate(inp) if c%2!=0]
+robo = [i for c, i in enumerate(inp) if c%2==0]
+
+def move(inp):
+    x,y = 0, 0
+    for i in inp:
+        if i == '>':
+            x+=1
+        elif i == '<':
+            x-=1
+        elif i == '^':
+            y-=1
+        elif i == 'v':
+            y+=1
+
+        locations.append((x,y))
+
+move(santa)
+move(robo)
 
 print(len(set(locations)))
