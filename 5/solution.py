faa6623673c4fdf6b6ababf058f783bd29266ccd
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-04-03 10:33:16 +0200

    day 5 part 2

--- 5/solution.py
+++ 5/solution.py
@@ -1,3 +1,5 @@
+import re
+
 def is_vowel(s:str) -> bool:
     """check if letter is a vowel"""
     vowels = ['a','e','i','o','u']
@@ -35,11 +37,36 @@ def get_number_of_nice_strings(inp):
                 counter += 1
 
     return counter
-                
+
+
+def contains_two_pairs_of_same_letter(s):
+    """
+    checks if string contains two pairs of same letters that not overlap with regex
+    (e.g.: abab, xxyxx, bcafhgjejkbc)
+    """
+    if re.search(r'(\w)(\w).*(\1)(\2)', s):
+        # print(re.search(r'(\w)(\w).*(\1)(\2)', s))
+        return True
+    return False
+
+def contains_same_letter_spaced_by_one(s):
+    """checks if string contains same letter spaced by any letter with regex (e.g.: yxy, aba)"""
+    if re.search(r'(\w)\w(\1)', s):
+        # print(re.search(r'(\w)\w(\1)', s))
+        return True
+    return False
+
+def get_number_of_nice_strings_2(inp):
+    """checks the input for number of the 'nice' strings"""
+    counter = 0
+    for s in inp:
+        if contains_same_letter_spaced_by_one(s) and contains_two_pairs_of_same_letter(s):
+            counter += 1
+    return counter
 
 if __name__ == '__main__':
     with open("input.txt", "r", encoding="utf-8") as f:
         inp = f.read().split()
 
-        print(get_number_of_nice_strings(inp))
-
+        print(f'Part one: {get_number_of_nice_strings(inp)}')
+        print(f'Part two: {get_number_of_nice_strings_2(inp)}')
